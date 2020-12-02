2e16dc9e46967243a2cd46f5e5ae9488cfaa9ac6
trodrigu <trodriguez91@icloud.com>
2020-12-01 19:02:32 -0800

    update default nix

--- default.nix
+++ default.nix
@@ -1,19 +1,20 @@
-{ mkDerivation, aeson, base, bytestring, concurrent-extra
-, http-client, http-client-tls, http-types, monad-logger, mtl
-, parallel-io, persistent, persistent-postgresql
-, persistent-template, scalpel, stdenv, text, time
+{ mkDerivation, aeson, base, bytestring, ghcide, http-client
+, http-client-tls, http-types, monad-logger, mtl, parallel-io
+, persistent, persistent-postgresql, persistent-template, scalpel
+, stdenv, text, time
 }:
 mkDerivation {
   pname = "camp-notify";
-  version = "0.1.0.0";
+  version = "0.1.0.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    aeson base bytestring concurrent-extra http-client http-client-tls
-    http-types monad-logger mtl parallel-io persistent
-    persistent-postgresql persistent-template scalpel text time
+    aeson base bytestring http-client http-client-tls http-types
+    monad-logger mtl parallel-io persistent persistent-postgresql
+    persistent-template scalpel text time
   ];
+  executableSystemDepends = [ ghcide ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
